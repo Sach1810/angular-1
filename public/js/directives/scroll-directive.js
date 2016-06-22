b1bdbdd68de5e4df101405e6d48f287cffe31bec
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-06-22 16:51:37 +0300

    Sections complete still need to fix contact section styling and make google no-captch and send emails

--- public/js/directives/scroll-directive.js
+++ public/js/directives/scroll-directive.js
@@ -84,8 +84,18 @@ myapp.directive("myNavscroll", function($window) {
 
 
 
-
-
+            // See if element is visible on screen
+            //
+            // function isScrolledIntoView(elem)
+            // {
+            //     var docViewTop = $(window).scrollTop();
+            //     var docViewBottom = docViewTop + $(window).height();
+            //
+            //     var elemTop = $(elem).offset().top;
+            //     var elemBottom = elemTop + $(elem).height();
+            //
+            //     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+            // }
 
 
 
